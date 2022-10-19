15ca10e128a83d043f04a88d0a40860b24555081
Fenil Patel <102594553+fbpatel003@users.noreply.github.com>
2022-10-20 00:11:56 +0530

    Add files via upload

--- src/Components/HomeCards.js
+++ src/Components/HomeCards.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React from "react";
+import images from "./images.jpg";
 
-function HomeCards (props) {
-    let curName = props.Cards[1].name;
-    return(
-        <>
-        {
-            curName
-        }
-        </>
-    )
+function HomeCards(props) {
+
+    // props.Cards.map((oneCard) => {
+    //     if(oneCard.price<2500) {
+    //         document.getElementById(oneCard.id).style.display = "block";
+    //     } else {
+    //         document.getElementById(oneCard.id).style.display = "none";
+    //     }
+    // }
+    // );
+
+  const listItems = props.Cards.map((oneCard) => (
+    <>
+      <div className="card col-md-2" style={{ width: "18rem" }} id={oneCard.id}>
+        <img src={images} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{oneCard.name}</h5>
+          <h5>${oneCard.price} <small>/month</small> </h5>
+          <h6>Type: {oneCard.type}</h6>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <strong>Location: {oneCard.location}</strong>
+          <br />
+          <a href="#" className="btn btn-primary">
+            Buy House
+          </a>
+        </div>
+      </div>
+    </>
+  ));
+
+  return <>{listItems}</>;
 }
 
 export default HomeCards;
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,23 +1,56 @@
 import React, { useState } from "react";
 import HomeCards from "./HomeCards";
-// import HomeCards from "./HomeCards";
 
 function HomePage() {
-
   let allCards = [
     {
+      id: 1,
       price: 2700,
       name: "Palm Harbour",
       location: "Jamnagar",
       imgSrc: "ok",
+      type: "Commercial",
     },
     {
+      id: 2,
       price: 2000,
       name: "Beverly",
       location: "Rajkot",
       imgSrc: "ok",
+      type: "Commercial",
+    },
+    {
+      id: 3,
+      price: 2100,
+      name: "new House",
+      location: "Jamnagar",
+      imgSrc: "ok",
+      type: "Residential",
     },
   ];
+
+  //   function setProp() {
+  //   // var selectElement = document.querySelector('#locationSelection');
+  //   // var output = selectElement.value;
+  //   // console.log(output);
+  //   $(".dropdown-menu li a").click(function(){
+  //     var selText = $(this).text();
+  //     $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+  //   });
+
+  // }
+  // var a;
+  // document.getElementById("locationSelection").addEventListener('change', ()=>{
+  //   a = document.getElementById("locationSelection").option(document.getElementById("locationSelection").selected).text;
+  //   console.log(a);
+  // })
+
+  // let e = document.getElementById("locationSelection");
+  // let text = e.options[e.selectedIndex].value;
+  // console.log(text);
+  // function setProps(){
+
+  // }
 
   return (
     <>
@@ -50,10 +83,9 @@ function HomePage() {
               aria-label="Default select example"
               id="locationSelection"
             >
-              <option selected>Open this select menu</option>
-              <option value={1}>One</option>
-              <option value={2}>Two</option>
-              <option value={3}>Three</option>
+              <option selected>Search by Location</option>
+              <option value={1}>Jamnagar</option>
+              <option value={2}>Rajkot</option>
             </select>
           </div>
 
@@ -61,10 +93,9 @@ function HomePage() {
             <div className="Move-in text-muted">When to move in</div>
             <br />
             <select className="form-select" aria-label="Default select example">
-              <option selected>Open this select menu</option>
-              <option value={1}>One</option>
-              <option value={2}>Two</option>
-              <option value={3}>Three</option>
+              <option selected>Search by date</option>
+              <option value={1}>19/10/'22</option>
+              <option value={2}>20/10/'22</option>
             </select>
           </div>
 
@@ -72,10 +103,10 @@ function HomePage() {
             <div className="price-range text-muted">Price Range</div>
             <br />
             <select className="form-select" aria-label="Default select example">
-              <option selected>Open this select menu</option>
-              <option value={1}>One</option>
-              <option value={2}>Two</option>
-              <option value={3}>Three</option>
+              <option selected>Search by Price</option>
+              <option value={1}>less then 2000</option>
+              <option value={2}>2000-3000</option>
+              <option value={3}>more then 3000</option>
             </select>
           </div>
 
@@ -83,19 +114,32 @@ function HomePage() {
             <div className="property-type text-muted">Property Type</div>
             <br />
             <select className="form-select" aria-label="Default select example">
-              <option selected>Open this select menu</option>
-              <option value={1}>One</option>
-              <option value={2}>Two</option>
-              <option value={3}>Three</option>
+              <option selected>Search by Type</option>
+              <option value={1}>Residential</option>
+              <option value={2}>Commercial</option>
             </select>
           </div>
 
-          <button type="button" class="btn btn-primary">
-            Primary
+          <button
+            type="button"
+            className="btn btn-primary btn-search"
+            onClick={() => {
+              var a;
+                  a = document
+                    .getElementById("locationSelection")
+                    .options(
+                      document.getElementById("locationSelection").selectedIndex
+                    ).text;
+                  console.log(a);
+            }}
+          >
+            Search
           </button>
         </div>
 
-        <HomeCards Cards={allCards}/>
+        <div className="raw d-flex justify-content-evenly card-raw">
+          <HomeCards Cards={allCards} />
+        </div>
       </div>
     </>
   );
